9238e16a1e85fda665e893eafbed6930cded8127
Erik Hebisch <flegelleicht@gmail.com>
2018-09-19 16:43:33 +0200

    Fixed typo

--- rails5/todos-app/config/routes.rb
+++ rails5/todos-app/config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   resources :todos do
     resources :items
   end
-  post 'auth/login', to 'authentication#authenticate'
+  post 'auth/login', to: 'authentication#authenticate'
 end
