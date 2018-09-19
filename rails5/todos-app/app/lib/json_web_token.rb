29143968cf0e014274fa7ecdcb228bd9dc6a7e53
Erik Hebisch <flegelleicht@gmail.com>
2018-09-19 16:37:06 +0200

    Added auth code and spec

--- rails5/todos-app/app/lib/json_web_token.rb
+++ rails5/todos-app/app/lib/json_web_token.rb
@@ -1,7 +1,7 @@
 class JsonWebToken
-  HMAC_SECRET = Rails.application.secrets.secret_key_base
+  HMAC_SECRET = Rails.application.credentials.secret_key_base
   
-  def self.encode(paylod, exp = 25.hours.from_now)
+  def self.encode(payload, exp = 25.hours.from_now)
     payload[:exp] = exp.to_i
     JWT.encode(payload, HMAC_SECRET)
   end
